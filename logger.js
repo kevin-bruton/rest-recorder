e60ff9e94b6e8d8a50e2ec0a52de4e0709545610
Kevin Bruton <kevin.jose.bs@gmail.com>
2019-09-30 18:11:51 +0200

    Add log to file possibility

--- logger.js
+++ logger.js
@@ -1,6 +1,18 @@
+const fs = require('fs')
+const path = require('path')
 let logOn = false
+let logToFile = false
 
 module.exports = {
   setLogOn: log => (logOn = log),
-  log: data => logOn && console.log(data)
+  setLogToFile: toFile => (logToFile = toFile),
+  log: data => {
+    if (logOn) {
+      console.log(data)
+      if (logToFile) {
+        const filepath = path.join(process.cwd(), 'log.txt')
+        fs.writeFileSync(filepath, data)
+      }
+    }
+  }
 }
