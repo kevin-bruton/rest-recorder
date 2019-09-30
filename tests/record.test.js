fc4db86a5ca3ba960b9615a7259578548d0d0bbd
Kevin Bruton <kevin.jose.bs@gmail.com>
2019-09-30 18:12:38 +0200

    Fix record test

--- tests/record.test.js
+++ tests/record.test.js
@@ -1,6 +1,4 @@
 /* global describe it expect beforeAll afterAll */
-
-const { fork } = require('child_process')
 const axios = require('axios')
 const path = require('path')
 const fs = require('fs')
@@ -9,27 +7,39 @@ const rimraf = require('rimraf')
 const config = require('./testConfig.js')
 
 describe('Making a request in RECORD MODE', () => {
-  let restRecorderProcess
   let realResponse
   let resp
+  let server
+  const pathUrl = '/api/users/2'
   beforeAll(async () => {
-    const configFilePath = path.join(__dirname, './testConfig.js')
-    restRecorderProcess = fork(path.join(__dirname, '..', '/index'), [`--configFile="${configFilePath}"`, '--mode=record'])
-    realResponse = await axios.get(config.remoteUrl)
-    resp = await axios.get('http://localhost:5555')
+    process.argv.push(`--configFile=${process.cwd()}/tests/testConfig.js`)
+    process.argv.push(`--mode=record`)
+    server = require('../index')
+    realResponse = await axios.get(config.remoteUrl + pathUrl)
+    resp = await axios.get('http://localhost:5555' + pathUrl)
   })
   afterAll(() => {
-    restRecorderProcess.kill()
+    server.close()
     rimraf.sync(config.restRecordingsDir)
   })
   it('returns the data from the remote', async () => {
     expect(resp.data).toStrictEqual(realResponse.data)
   })
   it('saves the request and response in file', () => {
+    const requestData = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json, text/plain, */*',
+        'user-agent': 'axios/0.19.0',
+        connection: 'close'
+      },
+      data: {},
+      url: config.remoteUrl + pathUrl
+    }
     const getRecordingFilePath = vcr.__get__('getRecordingFilePath')
-    const filepath = getRecordingFilePath(config.restRecordingsDir, config.remoteUrl, '/', { method: 'GET', data: {} }, data => data)
+    const filepath = getRecordingFilePath(config.restRecordingsDir, config.remoteUrl, pathUrl, requestData, data => data)
     const recording = JSON.parse(fs.readFileSync(filepath, 'utf8'))
-    expect(recording.request).toEqual({ method: 'GET', headers: {}, data: {}, url: config.remoteUrl })
+    expect(recording.request).toEqual(requestData)
     expect(recording.response).toEqual({ status: realResponse.status, statusText: realResponse.statusText, data: realResponse.data })
   })
 })
--- tests/testConfig.js
+++ tests/testConfig.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  remoteUrl: 'https://reqres.in/api/users/2',
+  remoteUrl: 'https://reqres.in',
   restRecorderPort: '5555',
   restRecordingsDir: path.join(__dirname, 'testRecordings'),
   log: false
